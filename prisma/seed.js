3d0c8280a1e021f35e2bca21a04398b15c084cb4
dryiel <73575387+dryiel@users.noreply.github.com>
2025-10-06 18:11:13 +0700

    Delete prisma directory

--- prisma/seed.js
+++
@@ -1,8 +0,0 @@
-const {PrismaClint} = require("@prisma/client");
-const bcrypt = require("bcrypt");
-
-const prisma = new PrismaClient();
-
-async function main(){
-  // hash password def
-const hashedPassword1 = await bcrypy.hash
